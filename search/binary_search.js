50e43f6a573c9d86d3c720bb14d7f9fd4414fc9c
Usenmfon <usenmfonuko@gmail.com>
2023-03-30 15:17:47 +0100

    added calls

--- search/binary_search.js
+++ search/binary_search.js
@@ -16,8 +16,8 @@ function binarySearch(arr, t){
     return -1
 }
 
-// console.log(binarySearch([-5,2,4,6,10], 10))
+console.log(binarySearch([-5,2,4,6,10], 10))
 console.log(binarySearch([-5,2,4,6,10], 6))
-// console.log(binarySearch([-5,2,4,6,10], 20))
+console.log(binarySearch([-5,2,4,6,10], 20))
 
 // Big O - O(logn)
